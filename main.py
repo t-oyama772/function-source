af13bcfadc80e98520183f4c88343b99d142a771
t-oyama772 <toshihide_oyama@crestive.jp>
2022-08-24 23:05:20 +0900

    messageを修正

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def hello_pubsub(event, context):
          context (google.cloud.functions.Context): Metadata for the event.
     """
     pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    pubsub_message = '---hogehoge ' + pubsub_message + ' hogehoge---'
+    pubsub_message = '--- ' + pubsub_message + ' hogehoge---'
     print(pubsub_message)
